c01cffbb4f3266ae71605b9d7becca6b58afeb1a
260058paisd <260058@paisd.net>
2025-02-13 09:12:19 -0600

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,46 +1,59 @@
 let car;
+let other
+let picker
+let wheel
 
 // Load the image and create a p5.Image object.
-function preload() {
+function preload()
+{
   car = loadImage('/assets/Car.png');
-  
+  other = loadImage('/assets/Other.png')
+  wheel = loadImage('/assets/Wheel.png')
+  car.loadPixels()
 }
 
-function setup() {
+function setup() 
+{
   createCanvas(500, 500);
   background(200)
-
   
+  picker = createColorPicker("red")
+  picker.position(0,0)
+
   car.resize(0,150)
-  // Draw the car.
-  
-  
-  car.loadPixels()
-  
-  // A double for loop. First we loop over the y, then the x
-  for (let y = 0; y < car.height; y++) {
-    for (let x = 0; x < car.width; x++) {
+  other.resize(0,150)
+  wheel.resize(0,80)
+}
 
-      // The equation for pixel index in a double loop is y * width + x
-      // Multiply by 4 since we have 4 color channels
-      const index = (y * car.width + x) * 4;
+function draw()
+{
+  colorizeCar()
+  car.updatePixels()
+  image(car, 75, 200);
+  image(other, 75, 200)
+  image(wheel, 90, 270)
+  image(wheel, 335, 270)
+}
 
-      // Get the individual color channels
-      const r = car.pixels[index + 0];
-      const g = car.pixels[index + 1];
-      const b = car.pixels[index + 2];
-      const a = car.pixels[index + 2];
+let myColor
 
-      // Do something with the pixels
-
-      // if red or green or blue is greater than 100, invert it
-      if (r < 100 || g < 100 || g < 100) {
-        car.pixels[index + 0] = 255 - r;
-        car.pixels[index + 1] = 255 - g;
-        car.pixels[index + 2] = 255 - b;
+//Color changing function.
+function colorizeCar()
+{
+  //Only run if new color has been selected.
+  if(myColor != picker.color())
+  {
+    myColor = picker.color()
+    //Iterate through every pixel.
+    for (let i = 0; i < car.pixels.length; i += 4) 
+    {
+      //Ignore transparent pixels.
+      if(car.pixels[1 + 3] != 1)
+      {
+        car.pixels[i] = red(myColor)
+        car.pixels[i + 1] = green(myColor)
+        car.pixels[i + 2] = blue(myColor)
       }
     }
   }
-  
-  image(car, 75, 200);
 }
